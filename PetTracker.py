f90aae1c33ee7e21fba6df58741de92c3aae5d04
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-30 14:54:01 +0300

    Created the functions needed to display info, mark adopted, and increment birthday.

--- PetTracker.py
+++ PetTracker.py
@@ -1,8 +1,18 @@
 class Pet:
-    # constructor
+  # Constructor for pet class
   def __init__(self,name, species, age):
     self.name = name
     self.species  = species
     self.age = age
     self.adopted = False
+   
+  # function to display pet info  
+  def display_info(self):
+      print(f"Pet name: {self.name}, Pet species: {self.species}, Pet Age: {self.age}, Adopted: {self.adopted}")
+      
+  def mark_adopted(self):
+      self.adopted = True
+    
+  def birthday(self):
+      self.age += 1
     
